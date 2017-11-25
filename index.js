cf96d9b5ba90d12907629199567b671b7deccaf8
Aron Simon <aron.simon.developer@gmail.com>
2017-11-25 18:35:49 +0100

    socket update

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-/*
+
 var socket = require('socket.io-client')('http://192.168.0.210:3000');
-
-socket.on('connect', function(){console.log("connected!")});
-socket.on('ls', function(){
-
-});
-socket.on('disconnect', function(){console.log("disconnected!")});
-*/
-var sleep = require("sleep");
+//var sleep = require("sleep");
 var exec = require('child_process').exec;
 
-console.log("kiscica")
-setInterval(()=>{
-//while(true) {
-//	sleep.sleep(2);	
+
+socket.on('connect', function(){console.log("connected!")});
+socket.on('update', function(){
 	exec('git pull', (err, stdout, stderr) => {
 	  if (err) {
 	    console.error(err);
@@ -21,6 +13,6 @@ setInterval(()=>{
 	  }
 	  console.log(stdout);
 	});
-//}
-},5000);
+});
+socket.on('disconnect', function(){console.log("disconnected!")});
 
